9847bf6c4699ad1e39629d102e593e203ab68165
Dji-F <cross.miron@gmail.com>
2022-02-23 02:03:47 +0300

    Styles checkbox finishing

--- src/modules/PassGen/templatePassGen.js
+++ src/modules/PassGen/templatePassGen.js
@@ -8,20 +8,36 @@ export const templatePassGen = () => {
                 <div id="select"></div>
             </div>
             <div class="passgen__text">Include numbers:</div>
-            <label for="number">
-                <input type="checkbox" name="number" checked>1234567890
-            </label>
+            <div>
+                <input id='number' type='checkbox' name="number" checked />
+                <label for='number'>1234567890</label>
+            </div>
             <div class="passgen__text">Include lowercase characters:</div>
-            <label for="lowercase">
-                <input type="checkbox" name="lowercase" checked>abcdef...
-            </label>
+            <div>
+                <input id="lowercase" type="checkbox" name="lowercase" checked />
+                <label for="lowercase">abcdef...</label>
+            </div>
             <div class="passgen__text">Include uppercase characters:</div>
-            <label for="uppercase">
-                <input type="checkbox" name="uppercase" checked>ABCDEF...
-            </label>
+            <div>
+                <input id="uppercase" type="checkbox" name="uppercase" checked />
+                <label for="uppercase">ABCDEF...</label>
+            </div>
             <button class="button-29" role="button" data-type="generator-btn">Generate password</button>
-<!--            <button data-type="generator-btn">Generate password</button>-->
             <input type="text" class="passgen__pass-value" data-type="pass-value">
+            <div class="close" data-type="close">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <svg viewBox="0 0 36 36" class="circle">
+                <path
+                  stroke-dasharray="100, 100"
+                  d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                />
+              </svg>
+            </div>
         </div>
     </div>
     `
